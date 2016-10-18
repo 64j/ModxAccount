378aaf6d0fcffec5aeaf8cfb174e61d3fbabbc9f
Serg <64j@mail.ru>
2016-10-19 00:46:21 +0300

    Update snippet.account.php

--- account/snippet.account.php
+++ account/snippet.account.php
@@ -1,15 +1,26 @@
 <?php
+/**
+ * modxAccount snippet
+ *
+ * @license GNU General Public License (GPL), http://www.gnu.org/copyleft/gpl.html
+ * @author ko4inn <ko4inn@gmail.com>
+ */
 
 if(!defined('MODX_BASE_PATH')) {
 	die('Unauthorized access.');
 }
 
 $config = array();
-$config['controller'] = !empty($controller) ? (is_numeric($controller) ? $modx->makeUrl($controller) : '/' . ltrim($controller, '/')) : 'register';
-$config['controllerRegister'] = !empty($controllerRegister) ? (is_numeric($controllerRegister) ? $modx->makeUrl($controllerRegister) : '/' . ltrim($controllerRegister, '/')) : 'register';
-$config['controllerLogin'] = !empty($controllerLogin) ? (is_numeric($controllerLogin) ? $modx->makeUrl($controllerLogin) : '/' . ltrim($controllerLogin, '/')) : 'login';
-$config['controllerForgot'] = !empty($controllerForgot) ? (is_numeric($controllerForgot) ? $modx->makeUrl($controllerForgot) : '/' . ltrim($controllerForgot, '/')) : 'forgot';
-$config['controllerProfile'] = !empty($controllerProfile) ? (is_numeric($controllerProfile) ? $modx->makeUrl($controllerProfile) : '/' . ltrim($controllerProfile, '/')) : 'profile';
+
+if(empty($controller)) {
+	$controller = $modx->documentIdentifier;
+}
+
+$config['controller'] = !empty($controller) ? (is_numeric($controller) ? $modx->makeUrl($controller) : '/' . ltrim($controller, '/')) : 'account';
+$config['controllerRegister'] = !empty($controllerRegister) ? (is_numeric($controllerRegister) ? $modx->makeUrl($controllerRegister) : '/' . ltrim($controllerRegister, '/')) : 'account/register';
+$config['controllerLogin'] = !empty($controllerLogin) ? (is_numeric($controllerLogin) ? $modx->makeUrl($controllerLogin) : '/' . ltrim($controllerLogin, '/')) : 'account';
+$config['controllerForgot'] = !empty($controllerForgot) ? (is_numeric($controllerForgot) ? $modx->makeUrl($controllerForgot) : '/' . ltrim($controllerForgot, '/')) : 'account/forgot';
+$config['controllerProfile'] = !empty($controllerProfile) ? (is_numeric($controllerProfile) ? $modx->makeUrl($controllerProfile) : '/' . ltrim($controllerProfile, '/')) : 'account/profile';
 $config['success'] = !empty($success) ? (is_numeric($success) ? $modx->makeUrl($success) : '/' . ltrim($success, '/')) : '';
 $config['userGroupId'] = !empty($userGroupId) ? $userGroupId : '';
 $config['keyVeriWord'] = time();
